130b9cd12197723129271542442b7b98e85cc679
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-03-11 16:15:07 +0000

    Allow the event inspector to throw exception

--- queue-client/src/main/java/tdl/participant/queue/connector/EventInspector.java
+++ queue-client/src/main/java/tdl/participant/queue/connector/EventInspector.java
@@ -2,5 +2,5 @@ package tdl.participant.queue.connector;
 
 @FunctionalInterface
 public interface EventInspector {
-    void inspect(String eventName, String eventVersion, Object eventObject);
+    void inspect(String eventName, String eventVersion, Object eventObject) throws Exception;
 }
