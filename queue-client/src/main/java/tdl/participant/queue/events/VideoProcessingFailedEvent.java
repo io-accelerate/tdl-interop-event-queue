58eb21ff467b00901b8b08c0d228d9090bc04215
neomatrix369 <sadhak001@gmail.com>
2019-01-30 23:59:24 +0000

    Add new event VideoProcessingFailedEvent to track video processing failures on the ECS

---
+++ queue-client/src/main/java/tdl/participant/queue/events/VideoProcessingFailedEvent.java
@@ -0,0 +1,21 @@
+package tdl.participant.queue.events;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Value;
+import tdl.participant.queue.connector.QueueEvent;
+
+@Value
+@QueueEvent(name = "videoProcessingFailed", version = "0.2")
+public class VideoProcessingFailedEvent implements ParticipantEvent {
+    private final long timestampMillis;
+    private final String participant;
+    private final String challengeId;
+
+    public VideoProcessingFailedEvent(@JsonProperty("timestampMillis") long timestampMillis,
+                                      @JsonProperty("participant") String participant,
+                                      @JsonProperty("challengeId") String challengeId) {
+        this.timestampMillis = timestampMillis;
+        this.participant = participant;
+        this.challengeId = challengeId;
+    }
+}
